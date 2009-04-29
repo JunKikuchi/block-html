f5036340f27463718ea1d1133e0e6240e5989885
菊池 淳 <kikuchi@bonnou.com>
2009-04-29 22:38:26 +0900

    Innate サポート追加

---
+++ lib/block-html/innate.rb
@@ -0,0 +1,19 @@
+module Innate
+  module View
+    register 'Innate::View::BlockHTML', :bhtml
+
+    module BlockHTML
+      def self.call(action, string)
+        string = transform_string(action, string) if action.view
+        return string, 'text/html; charset=utf-8'
+      end
+
+      def self.transform_string(action, string)
+        action.instance.instance_eval do
+          args = action.params
+          instance_eval(string)
+        end
+      end
+    end
+  end
+end
